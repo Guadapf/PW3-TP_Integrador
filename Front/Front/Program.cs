41803de90b41bb139563e9d41f35d99615970ff5
ibieldahuk <ibieldahuk@gmail.com>
2024-06-05 10:13:42 -0300

    Página Home en el proyecto Front

--- Front/Front/Program.cs
+++ Front/Front/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Empleado}/{action=Details}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
