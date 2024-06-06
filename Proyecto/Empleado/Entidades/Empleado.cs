b0cda32226535560a68eb69a67f435d094b00641
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-06 13:04:03 -0300

    scaffold dbfirst

--- Proyecto/Empleado/Entidades/Empleado.cs
+++ Proyecto/Empleado/Entidades/Empleado.cs
@@ -1,8 +1,29 @@
-﻿namespace Entidades
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades;
+
+public partial class Empleado
 {
-    public class Empleado
-    {
-        public int idEmpleado { get; set; }
-        public string Nombre { get; set; }
-    }
+    public int IdEmpleado { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public DateOnly FechaNac { get; set; }
+
+    public DateOnly FechaIngreso { get; set; }
+
+    public int IdGenero { get; set; }
+
+    public int IdPais { get; set; }
+
+    public int IdDepartamento { get; set; }
+
+    public virtual Departamento IdDepartamentoNavigation { get; set; } = null!;
+
+    public virtual Genero IdGeneroNavigation { get; set; } = null!;
+
+    public virtual Pai IdPaisNavigation { get; set; } = null!;
 }
---
+++ Proyecto/Empleado/Entidades/Pai.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades;
+
+public partial class Pai
+{
+    public int IdPais { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public virtual ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+}
