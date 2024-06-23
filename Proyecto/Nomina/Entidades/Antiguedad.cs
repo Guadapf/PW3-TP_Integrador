9d7130e74741015008e5d2e239d351aca651562d
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-23 12:25:48 -0300

    scaffold

---
+++ Proyecto/Nomina/Entidades/Antiguedad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nomina;
+
+public partial class Antiguedad
+{
+    public int IdAntiguedad { get; set; }
+
+    public int? Anios { get; set; }
+
+    public decimal? Bono { get; set; }
+}
---
+++ Proyecto/Nomina/Entidades/Compensacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nomina;
+
+public partial class Compensacion
+{
+    public int IdCompensacion { get; set; }
+
+    public int? IdDepartamento { get; set; }
+
+    public decimal? Multiplicador { get; set; }
+}
