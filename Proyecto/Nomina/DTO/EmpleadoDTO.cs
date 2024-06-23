eab7690cf0711e8fb66680cab9736636a8a764c7
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-23 12:31:05 -0300

    dto

---
+++ Proyecto/Nomina/DTO/EmpleadoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO;
+
+public class EmpleadoDTO
+{
+    public int IdEmpleado { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public DateOnly FechaIngreso { get; set; }
+    public int IdDepartamento { get; set; }
+    public int IdPais { get; set; }
+    public decimal Salario { get; set; }
+}
