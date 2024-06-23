60231c05ca5d76f0876f01b026d26cd26d721ae2
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-23 12:26:47 -0300

    configs

--- Proyecto/Nomina/Nomina/Program.cs
+++ Proyecto/Nomina/Nomina/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Nomina;
+using Repositorio;
+using Servicio;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<NominaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NominaContext")));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ISalarioBaseRepository, SalarioBaseRepository>();
+builder.Services.AddTransient<ISalarioBaseService, SalarioBaseService>();
 
 var app = builder.Build();
 
