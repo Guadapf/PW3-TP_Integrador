c2922377e2ec43f9b05fabe582b50148294282b2
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-23 12:26:17 -0300

    add service

---
+++ Proyecto/Nomina/Servicio/SalarioBaseService.cs
@@ -0,0 +1,31 @@
+﻿using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio;
+
+public interface ISalarioBaseService
+{
+    decimal CalcularSalario(int idPais, int idDepartamento, DateOnly fechaIngreso);
+}
+public class SalarioBaseService : ISalarioBaseService
+{
+   private readonly ISalarioBaseRepository _salarioBaseRepository;
+
+    public SalarioBaseService(ISalarioBaseRepository salarioBaseRepository)
+    {
+        _salarioBaseRepository = salarioBaseRepository;
+    }
+
+    public decimal CalcularSalario(int idPais, int idDepartamento, DateOnly fechaIngreso)
+    {
+        var salarioBase = _salarioBaseRepository.ObtenerSalarioBase(idPais);
+        var compensacion = _salarioBaseRepository.ObtenerCompensacion(idDepartamento);
+        var antiguedad = _salarioBaseRepository.ObtenerAntiguedad(fechaIngreso);
+
+        return (salarioBase * compensacion) + antiguedad;
+    }
+}
