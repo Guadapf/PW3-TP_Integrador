bb53215b7e9897a5e12d9625a7dedd2e2a8c815e
Fernandez Guadalupe <guadapfernandez@gmail.com>
2024-06-23 14:27:15 -0300

    add controller

---
+++ Proyecto/ApiGateway/ApiGateway/Controllers/NominaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NominaController : ControllerBase
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public NominaController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CalcularSalario(int idPais, int idDepartamento, DateOnly fechaIngreso)
+    {
+        var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://localhost:7254/api/nomina/CalcularSalario?IdPais=idPais&IdDepartamento=idDepartamento&FechaIngreso=fechaIngreso")
+        {
+            Headers =
+                {
+                    {"Accept", "application/json" },
+                    {"User-Agent", "HttpRequestsSample" }
+                }
+        };
+
+        var myClientINC = _httpClientFactory.CreateClient();
+        var response = myClientINC.Send(httpRequestMessage);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        if (response.IsSuccessStatusCode)
+        {
+            return Content(content, "application/json");
+        }
+        else
+        {
+            return StatusCode((int)response.StatusCode, "mission failed, we'll get 'em next time");
+        }
+
+    }
+}
