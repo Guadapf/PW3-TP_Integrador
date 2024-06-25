d84e52ecd562efc8d9c8ef8a0e10c540b94a4af2
ibieldahuk <ibieldahuk@gmail.com>
2024-06-25 13:57:36 -0300

    Vista detalle p2

--- Front/Front/Controllers/NominaController.cs
+++ Front/Front/Controllers/NominaController.cs
@@ -14,28 +14,4 @@ public class NominaController : Controller
     {
         return View();
     }
-
-    [HttpPost]
-    public IActionResult DetalleEmpleado(BusquedaModel parametro)
-    {
-        EmpleadoNominaModel model = new EmpleadoNominaModel()
-        {
-            Nombre = "Eduardo",
-            Apellido = "Caracas",
-            Edad = 12,
-            Antiguedad = 14,
-            GeneroDescripcion = "L",
-            PaisDescripcion = "Argentina",
-            DepartamentoDescripcion = "La Matanza",
-            Base = 700000.00f,
-            Compensacion = 1.25f,
-            Bono = 50000.00f,
-            Total = 925000.00f
-        };
-        List<EmpleadoNominaModel> empleados = new List<EmpleadoNominaModel>();
-        empleados.Add(model);
-        ViewBag.Resultados = empleados;
-        ViewBag.Empleado = model;
-        return View();
-    }
 }
